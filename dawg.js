dd60f4aa8df4158175586c762fe979990a23bd0f
slimeVidas26 <dahan.isaac@gmail.com>
2018-10-22 21:06:20 +0300

    show dogs img

--- dawg.js
+++ dawg.js
@@ -1,38 +1,61 @@
-$(function () {
+(function() {
     var $breedSelection = null;
+    var $img = null;
 
     var init = function () {
         $('#ctr').html(`
 
     <div class="form-group">
   <select class="form-control">
-  <option>Select breed</option>
+  <option value="">Select breed</option>
   </select>
 </div>
  
 
    <p> 
-    <button type="button" class=" btn btn-primary">Go</button>
+    <button type="button" class="btn btn-primary">Go</button>
     </p>
-    
-    
+    <div class="img">
+    </div>
     `);
+    $breedSelection = $('#ctr > div > select');
+    $img = $('#ctr .img');
+    $info = $('.info');
+    $info.hide();
     getBreeds();
     }
 
     var loadBreedToSelect = function(s){
-        $breedSelection = $('#ctr div select');
+        
         for(var i in s){
-            $breedSelection.append(`<option>${i}</option>`);
+            //console.log('i',i)
+            //console.log('s[i]' , s[i])
+            $breedSelection.append(`<option class="bold">${i}</option>`);
             if(s[i].length){
-                for(var ii=0; ii< s[i].length;ii++ ){
+                for(var ii=0; ii<s[i].length;ii++){
                     console.log(i+'/'+s[i][ii]);
-                    $breedSelection.append(`<option value="${i}/${s[i][ii]}">&raquo;&nbsp;${s[i][ii]}</option>`);
-
+                    $breedSelection.append(`<option value = "${i}/${s[i][ii]}">&raquo;&nbsp;${s[i][ii]}</option>`);
                 }
-            }
+            }  
 
         }
+        $('#ctr > p > button').on('click',function(){
+            console.log($breedSelection.val());
+            var v = $breedSelection.val();
+            if(v!=''){
+              getImage(v);
+              $info.hide();
+            }
+            else{
+             $img.hide(1000)
+             $info.html(`Choose breed`).show( "slow" );
+            }
+        });
+    }
+
+    var loadImageToCtr = function(url){
+        console.log('loadImageToCtr',url);
+        $img.html(`<img src="${url}">`).show("slow" );
     }
 
     var getBreeds = function(){
@@ -41,13 +64,29 @@ $(function () {
             method : "GET",
             dataType : "json"
         }).done(function(r){
-            console.log(r.message);
+           // console.log(r.message);
             loadBreedToSelect(r.message);  
-        })
-    }
+        }).fail(function(x , msg){
+          console.log('error:',msg)
+        });
 
+       
+    };
+
+    var getImage = function(breed){
+        $.ajax({
+            url:`https://dog.ceo/api/breed/${breed}/images/random`,
+            method:'GET',
+            dataType:'json'
+        }).done(function(ret){
+           console.log(ret);
+           loadImageToCtr(ret.message);
+        }).fail(function(x,msg){
+           console.log('error:',msg)
+        });
+    }
 
 
     init();
 
-});
+})();
